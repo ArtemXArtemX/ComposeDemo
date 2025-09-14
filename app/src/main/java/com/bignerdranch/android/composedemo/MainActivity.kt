8a58dbad86a74c3a514481c9739230811d05a9cc
tematrkv <tatarnikov.stud@mail.ru>
2025-09-14 13:52:36 +0700

    Added the composable of the DemoScreen element

--- app/src/main/java/com/bignerdranch/android/composedemo/MainActivity.kt
+++ app/src/main/java/com/bignerdranch/android/composedemo/MainActivity.kt
@@ -5,14 +5,25 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Slider
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableFloatStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 import com.bignerdranch.android.composedemo.ui.theme.ComposeDemoTheme
 
@@ -44,4 +55,40 @@ class MainActivity : ComponentActivity() {
             DemoText(message = "Welcome to Android", fontSize = 12f)
         }
     }
+
+    @Composable
+    fun DemoSlider(sliderPosition: Float, onPositionChange: (Float) -> Unit) {
+        Slider(
+            modifier = Modifier.padding(10.dp),
+            valueRange = 20f..38f,
+            value = sliderPosition,
+            onValueChange = { onPositionChange(it) }
+        )
+    }
+
+    @Composable
+    fun DemoScreen(modifier: Modifier = Modifier) {
+        var sliderPosition by remember { mutableFloatStateOf(20f) }
+        val handlePositionChange = { position: Float ->
+            sliderPosition = position
+        }
+        Column(
+            horizontalAlignment = Alignment.CenterHorizontally,
+            verticalArrangement = Arrangement.Center,
+            modifier = Modifier.fillMaxSize()
+        ) {
+            DemoText(message = "Welcome to Compose", fontSize = sliderPosition)
+            Spacer(modifier = Modifier.height(150.dp))
+            DemoSlider(
+                sliderPosition = sliderPosition,
+                onPositionChange = handlePositionChange
+            )
+            Text(
+                style = MaterialTheme.typography.headlineMedium,
+                text = sliderPosition.toInt().toString() + "sp"
+            )
+        }
+    }
 }
+
+
