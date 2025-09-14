137e8a8a313224259b5f325a69743398fc0e56ef
tematrkv <tatarnikov.stud@mail.ru>
2025-09-14 14:24:52 +0700

    Final commit

--- app/src/main/java/com/bignerdranch/android/composedemo/MainActivity.kt
+++ app/src/main/java/com/bignerdranch/android/composedemo/MainActivity.kt
@@ -1,6 +1,5 @@
 package com.bignerdranch.android.composedemo
 
-import android.R.id.message
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
@@ -19,6 +18,7 @@ import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableFloatStateOf
 import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.text.font.FontWeight
@@ -34,61 +34,60 @@ class MainActivity : ComponentActivity() {
         setContent {
             ComposeDemoTheme {
                 Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
+                    DemoScreen(modifier = Modifier.padding(innerPadding))
                 }
             }
         }
     }
+}
 
-    @Composable
-    fun DemoText(message: String, fontSize: Float) {
+@Composable
+fun DemoText(message: String, fontSize: Float) {
+    Text(
+        text = message,
+        fontSize = fontSize.sp,
+        fontWeight = FontWeight.Bold
+    )
+}
+
+@Composable
+fun DemoSlider(sliderPosition: Float, onPositionChange: (Float) -> Unit) {
+    Slider(
+        modifier = Modifier.padding(10.dp),
+        valueRange = 20f..38f,
+        value = sliderPosition,
+        onValueChange = { onPositionChange(it) }
+    )
+}
+
+@Composable
+fun DemoScreen(modifier: Modifier = Modifier) {
+    var sliderPosition by remember { mutableFloatStateOf(20f) }
+
+    Column(
+        horizontalAlignment = Alignment.CenterHorizontally,
+        verticalArrangement = Arrangement.Center,
+        modifier = modifier.fillMaxSize()
+    ) {
+        DemoText(message = "Welcome to Compose", fontSize = sliderPosition)
+        Spacer(modifier = Modifier.height(150.dp))
+        DemoSlider(
+            sliderPosition = sliderPosition,
+            onPositionChange = { newValue -> sliderPosition = newValue }
+        )
         Text(
-            text = message,
-            fontSize = fontSize.sp,
-            fontWeight = FontWeight.Bold
+            style = MaterialTheme.typography.headlineMedium,
+            text = sliderPosition.toInt().toString() + "sp"
         )
     }
+}
 
-    @Composable
-    fun DemoSlider(sliderPosition: Float, onPositionChange: (Float) -> Unit) {
-        Slider(
-            modifier = Modifier.padding(10.dp),
-            valueRange = 20f..38f,
-            value = sliderPosition,
-            onValueChange = { onPositionChange(it) }
-        )
-    }
-
-    @Composable
-    fun DemoScreen(modifier: Modifier = Modifier) {
-        var sliderPosition by remember { mutableFloatStateOf(20f) }
-        val handlePositionChange = { position: Float ->
-            sliderPosition = position
-        }
-        Column(
-            horizontalAlignment = Alignment.CenterHorizontally,
-            verticalArrangement = Arrangement.Center,
-            modifier = Modifier.fillMaxSize()
-        ) {
-            DemoText(message = "Welcome to Compose", fontSize = sliderPosition)
-            Spacer(modifier = Modifier.height(150.dp))
-            DemoSlider(
-                sliderPosition = sliderPosition,
-                onPositionChange = handlePositionChange
-            )
-            Text(
-                style = MaterialTheme.typography.headlineMedium,
-                text = sliderPosition.toInt().toString() + "sp"
-            )
-        }
-    }
-
-    @Preview(showSystemUi = true)
-    @Composable
-    fun DemoTextPreview() {
-        ComposeDemoTheme {
-            Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
-                DemoScreen(modifier = Modifier.padding(innerPadding))
-            }
+@Preview(showSystemUi = true)
+@Composable
+fun DemoTextPreview() {
+    ComposeDemoTheme {
+        Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
+            DemoScreen(modifier = Modifier.padding(innerPadding))
         }
     }
 }
